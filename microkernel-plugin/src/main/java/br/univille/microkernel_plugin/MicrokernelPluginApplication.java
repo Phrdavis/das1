1cc93b15d41448752c6c5806c2ecd98236b55d35
Phrdavis <davipi_sou@hotmail.com>
2024-09-30 19:42:14 -0300

    New project

---
+++ microkernel-plugin/src/main/java/br/univille/microkernel_plugin/MicrokernelPluginApplication.java
@@ -0,0 +1,13 @@
+package br.univille.microkernel_plugin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrokernelPluginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrokernelPluginApplication.class, args);
+	}
+
+}
---
+++ microkernel-plugin/src/test/java/br/univille/microkernel_plugin/MicrokernelPluginApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.microkernel_plugin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrokernelPluginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
